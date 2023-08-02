997e0d613163e5852db94aa255ab18e9c663355d
Allen Feng <haha30932@gmail.com>
2023-08-02 16:29:23 +0800

    add brick

--- app.js
+++ app.js
@@ -10,12 +10,71 @@ let ySpeed = 20;
 let ground_x = 100;
 let ground_y = 500;
 let ground_height = 5;
+let brickArray = [];
+
+function getRandomArbitrary(min, max) {
+  return min + Math.floor(Math.random() * (max - min));
+}
+
+class Brick {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 50;
+    this.height = 50;
+    brickArray.push(this);
+  }
+
+  drawBrick() {
+    ctx.fillStyle = "lightgreen";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  touchingBall(ball_x, ball_y) {
+    return (
+      ball_x >= this.x - radius &&
+      ball_x <= this.x + this.width + radius &&
+      ball_y <= this.y + this.height + radius &&
+      ball_y >= this.y - radius
+    );
+  }
+}
+
+for (let i = 0; i < 10; i++) {
+  new Brick(getRandomArbitrary(0, 950), getRandomArbitrary(0, 550));
+}
 
 c.addEventListener("mousemove", (e) => {
   ground_x = e.clientX;
 });
 
 function drawCircle() {
+  // 確認是否打到磚塊
+  brickArray.forEach((brick, index) => {
+    if (brick.touchingBall(circle_x, circle_y)) {
+      // 從下方撞擊
+      if (circle_y >= brick.y + brick.height) {
+        ySpeed *= -1;
+      }
+      // 從上方撞擊
+      else if (circle_y <= brick.y) {
+        ySpeed *= -1;
+      }
+      // 從左方撞擊
+      else if (circle_x <= brick.x) {
+        xSpeed *= -1;
+      }
+      // 從右方撞擊
+      else if (circle_x >= brick.x + brick.width) {
+        xSpeed *= -1;
+      }
+      brickArray.splice(index, 1);
+      if (brickArray.length == 0) {
+        alert("遊戲結束");
+        clearInterval(game);
+      }
+    }
+  });
   if (
     circle_x >= ground_x - radius &&
     circle_x <= ground_x + 200 + radius &&
@@ -48,6 +107,10 @@ function drawCircle() {
 
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+  brickArray.forEach((brick) => {
+    brick.drawBrick();
+  });
 
   ctx.fillStyle = "orange";
   ctx.fillRect(ground_x, ground_y, 200, ground_height);
